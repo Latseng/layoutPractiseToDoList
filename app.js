5a3fb5bb366524ce05051268cf95131b2fec7045
Lala Tseng <z162614@gmail.com>
2023-11-07 12:33:21 +0800

    function correction

--- app.js
+++ app.js
@@ -1,54 +1,63 @@
-let toDo = document.querySelector(".todo");
-let addText = document.querySelector(".add-item");
+const toDo = document.querySelector(".todo");
+const addText = document.querySelector(".add-item");
 
 //新增待辦事項列
-let addButton = document.querySelector(".add-btn");
+const addButton = document.querySelector(".add-btn");
 addButton.addEventListener("click", () => {
-  let newList = document.createElement("li");
+  const newList = document.createElement("li");
   newList.classList.add("item");
   toDo.appendChild(newList);
 
-  let newP = document.createElement("p");
+  const newP = document.createElement("p");
   newP.classList.add("text");
   newP.textContent = addText.value;
   newList.appendChild(newP);
 
-  let newEditButton = document.createElement("button");
+  const newEditButton = document.createElement("button");
   newEditButton.classList.add("edit-btn");
   newList.appendChild(newEditButton);
 
-  let newEditIcon = document.createElement("i");
+  const newEditIcon = document.createElement("i");
   newEditIcon.classList.add("fas");
   newEditIcon.classList.add("fa-edit");
   newEditIcon.classList.add("edit-icon");
   newEditButton.appendChild(newEditIcon);
 
-  let newDeleteButton = document.createElement("button");
+  const newDeleteButton = document.createElement("button");
   newDeleteButton.classList.add("delete-btn");
-  let newDeleteIcon = document.createElement("i");
+  const newDeleteIcon = document.createElement("i");
   newDeleteIcon.classList.add("fas");
   newDeleteIcon.classList.add("fa-trash");
   newDeleteIcon.classList.add("trash-icon");
   newDeleteButton.appendChild(newDeleteIcon);
   newList.appendChild(newDeleteButton);
 
-  const editItem = document.querySelectorAll(".edit-btn");
-  editItem.forEach((edit) => {
-    edit.addEventListener("click", (e) => {
-      const newText = prompt("請輸入新的記事：");
-      e.target.parentElement.parentElement.querySelector(".text").textContent =
-        newText;
+  //edit 事件
+  newEditButton.addEventListener("click", () => {
+    const newInput = document.createElement("input");
+    newInput.classList.add("item");
+    toDo.appendChild(newInput);
+    newList.classList.remove("item");
+    newList.classList.add("none");
+    newInput.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        newP.textContent = newInput.value;
+        newInput.classList.remove("item");
+        newInput.classList.add("none");
+        newList.classList.remove("none");
+        newList.classList.add("item");
+      }
     });
   });
 
-  newDeleteButton.addEventListener("click", (e) => {
-    e.target.parentElement.parentElement.classList.add("remove");
-    let list = e.target.parentElement.parentElement;
-    list.addEventListener("trasitionend", (e) => {
-      e.target.remove();
+  //delete 事件
+  newDeleteButton.addEventListener("click", () => {
+    newList.classList.add("remove");
+    newList.addEventListener("transitionend", () => {
+      newList.remove();
     });
   });
-  newList.style.animation = "scaleUp 0.5s ease forwards";
+  addText.value = "";
 });
 
 //製作編輯按鈕
@@ -62,12 +71,12 @@ editItem.forEach((edit) => {
 });
 
 //製作刪除項目按鈕
-let deleteItem = document.querySelectorAll(".delete-btn");
+const deleteItem = document.querySelectorAll(".delete-btn");
 deleteItem.forEach((trash) => {
   trash.addEventListener("click", (e) => {
     e.target.parentElement.parentElement.classList.add("remove");
   });
-  let list = trash.parentElement.parentElement;
+  const list = trash.parentElement.parentElement;
   list.addEventListener("transitionend", (e) => {
     e.target.remove();
   });
